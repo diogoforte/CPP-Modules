58c0727090b33d4c03ce568c41f683cc98eb236d
Diogo Jorge Nunes Gil Forte <dinunes-@c2r7s7.42lisboa.com>
2024-02-11 06:45:25 +0000

    Fixed compiling error

--- Module_07/ex01/src/main.cpp
+++ Module_07/ex01/src/main.cpp
@@ -7,6 +7,6 @@ int main() {
   int array[] = {1, 2, 3, 4, 5};
   size_t length = sizeof(array) / sizeof(array[0]);
   iter(array, length, increment);
-  for (int i = 0; i < length; ++i)
+  for (size_t i = 0; i < length; ++i)
     std::cout << array[i] << ' ';
 }
